ec6fae2f13687f15bad40548906592edb28ac023
SSU\yb9854 <yb9854@naver.com>
2022-01-06 15:23:12 +0900

    [FEAT] Data Binding

--- app/src/main/java/org/sopt/havit_study/MainActivity.kt
+++ app/src/main/java/org/sopt/havit_study/MainActivity.kt
@@ -2,10 +2,21 @@ package org.sopt.havit_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.sopt.havit_study.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var text = "TEST"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.activity=this
+        binding.btnChange.setOnClickListener {
+            text = "TEST2"
+            binding.invalidateAll()
+        }
     }
 }
