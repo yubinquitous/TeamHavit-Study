b469b89def75761b2ad5573c1ada4acd154716fe
yubinquitous <yb9854@naver.com>
2022-01-06 16:20:54 +0900

    [FEAT] DataBinding, LiveData

--- app/src/main/java/org/sopt/havit_study/MainActivity.kt
+++ app/src/main/java/org/sopt/havit_study/MainActivity.kt
@@ -3,20 +3,30 @@ package org.sopt.havit_study
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import org.sopt.havit_study.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    var text = "TEST"
+//    var text = "TEST"
+    val liveText = MutableLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.activity=this
-        binding.btnChange.setOnClickListener {
-            text = "TEST2"
-            binding.invalidateAll()
+        binding.apply {
+            lifecycleOwner = this@MainActivity
+            activity = this@MainActivity
+
+            btnChange.setOnClickListener {
+                if (liveText.value == "Hello DataBinding!")
+                    liveText.value = "Hello LiveData!"
+                else
+                    liveText.value = "Hello DataBinding!"
+            }
         }
+        liveText.value = "Hello DataBinding!"
     }
 }
